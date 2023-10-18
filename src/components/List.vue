41cad281a9cb7a5ddf2cd7a7bdd60a8735723830
usjamil <usjamil@squaretrade.com>
2023-10-18 20:44:19 +0500

    Vuejs Todo App

---
+++ src/components/List.vue
@@ -0,0 +1,34 @@
+<template>
+
+<div v-for="(t, i) in list" :key="i" :class="['list-container',t.completed ? 'completed' : '']"  @click="$emit('toggle-complete',i)">
+
+    <span>{{t.task}}</span><button @click="$emit('delete-task',i)">Delete</button>
+
+</div>
+
+</template>
+
+
+<script>
+export default {
+    name:"ListComp",
+    props:['list']
+
+}
+</script>
+
+
+<style scoped>
+.list-container{
+    display:flex;
+    gap:10px;
+    justify-content:space-between;
+    border:1px dashed green;
+    width:50%;
+    padding:10px;
+    cursor:pointer;
+}
+.completed{
+    border-style:solid;
+}
+</style>
---
+++ src/components/TodoList.vue
@@ -0,0 +1,79 @@
+<template>
+<div class="container">
+<div class="input-add">
+<input v-model="input" placeholder="Add task" /> <button @click="add()"> Add</button>
+</div>
+<List :list="todolist" @delete-task="deleteTask" @toggle-complete="toggleComplete"/>
+</div>
+</template>
+
+
+<script>
+import List from './List.vue'
+export default {
+    name:"TodoList",
+    components:{
+        List
+    },
+    data(){
+        return{
+            todolist:[{
+                task:"Today task",completed:false,
+
+            },
+            {
+                task:"Tomorrow task",completed:true,
+                
+            }],
+            input:""
+        }
+    },
+    methods:{
+        add(){
+
+            if(this.input){
+                console.log("innn")
+            this.todolist.push({task:this.input,completed:false})
+            }
+        },
+        deleteTask(e){
+            this.todolist.splice(e,1)
+        },
+        toggleComplete(e){
+            if(this.todolist.length>0)
+            this.todolist[e].completed = !(this.todolist[e]?.completed)
+        }
+    },emits:['delete-task','toggle-complete']
+}
+</script>
+
+
+<style scoped>
+.container{
+    width:50%;
+    display:flex;
+    flex-direction:column;
+    gap:10px;
+    border:1px solid black;
+    padding:10px;
+
+}
+input{
+    padding:10px;
+    border:1px solid grey;
+    margin-bottom:10px;
+
+}
+.input-add{
+    display:flex;
+    gap:10px;
+}
+button{
+    border:1px solid black;
+    padding:0px 0px;
+    background-color:transparent;
+}
+.input-add input{
+    width:100%;
+}
+</style>
